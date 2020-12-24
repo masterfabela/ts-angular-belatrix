472cf1b5fdbbffcf2c806dc31bddf76273fff928
Jose Ramon Vieites Romay <fabelaretorcida@gmail.com>
2020-12-24 17:52:02 +0100

    Clase 9

---
+++ function-type.ts
@@ -0,0 +1,18 @@
+
+type CallBackError = Error | null;
+type CallBack = (error: CallBackError, response:Object) => void;
+type SendRequest = (cb: CallBack) => void;
+
+const sendRequest:SendRequest = (cb: CallBack): void => {
+  if (cb) {
+    cb(null,{message: 'Todo salio como lo planeamos'});
+  }
+}
+
+function metodo(){
+
+}
+
+sendRequest(function() {
+
+});
